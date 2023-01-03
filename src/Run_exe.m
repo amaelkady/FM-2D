97281129a448f10929b2e2132d018c6751047043
Ahmed Elkady <a.elkady@soton.ac.uk>
2023-01-03 08:38:59 +0000

    - src removed for yearly update

--- src/Run_exe.m
+++
@@ -1,9 +0,0 @@
-function [RunTime]=Run_exe(OpenSEESFileName,ShowOpenseesStatus)
-
-if ShowOpenseesStatus==1  
-	eval(strcat('! OpenSees.exe ', OpenSEESFileName));
-else
-	[status, screenOutfile] = dos([['OpenSees.exe ', num2str(OpenSEESFileName)]]);
-end
-
-RunTime=toc;
