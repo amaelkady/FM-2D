05e11c03c5edd7081752e52c99e973ab71ebacf1
Ahmed Elkady <a.elkady@soton.ac.uk>
2026-02-04 13:44:11 +0000

    -v3.2602

--- src/Initialize_Project.m
+++ src/Initialize_Project.m
@@ -8,7 +8,7 @@ xx=xx(1:end-4);
 xx= xx(~isspace(xx)); % remove spaces from project name to get opensees file name
 OpenSEESFileName = [xx,'.tcl'];
 
-Version             = 3.2601;
+Version             = 3.2602;
 BuildOption         = 2;
 ExecutionOption     = 1;
 Uncertainty         = 0;
